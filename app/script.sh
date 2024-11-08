bc45230f3a4a6450e58cc2d508e02722c4264bc2
Etienne Binginot <etienne.binginot@gmail.com>
2024-11-08 14:13:40 +0100

    feat: UI integration base for ejb

--- app/script.sh
+++ app/script.sh
@@ -1,5 +1,7 @@
+mkdir /build
 for d in */ ; do
     cd $d
     mvn clean package -DskipTests
+    cp target/*.{jar,war} /build/
     cd ../
 done
---
+++ app/ui-ejb/src/main/java/org/uiejb/uiejb/UiEjbApplication.java
@@ -0,0 +1,13 @@
+package org.uiejb.uiejb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiEjbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiEjbApplication.class, args);
+    }
+
+}
